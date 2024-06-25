d23d5a443053fa9a5fc1b7b56b9b1bfe1ddd1ab4
DolceTriade <vcelestialragev@gmail.com>
2024-06-24 21:36:16 -0700

    lua: Add str.join

--- game/lua/str.lua
+++ game/lua/str.lua
@@ -5,8 +5,23 @@ function str.split(input, sep)
         sep = "%s"
     end
     local t = {}
-    for s in string.gmatch(input, "([^" .. sep .. "]+)") do
+    for s in string.gmatch(input, '([^' .. sep .. ']+)') do
         table.insert(t, s)
     end
     return t
 end
+
+function str.join(input, sep)
+    local s = ''
+    local first = true
+    for _, v in ipairs(input) do
+        if first then
+           first = false
+        else
+            s = s .. sep
+        end
+        s = s .. v
+    end
+end
+
+return str
